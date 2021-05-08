ba199f6adf8361422be0952315e4997abaa98aa4
Asurada520 <602423940@qq.com>
2021-05-08 17:43:52 +0800

    spring cloud zuul rateLimiter and gray test

---
+++ service-zuul-ratelimiter-eureka-9000/src/main/java/com/jade/serviceeureka/ServiceZuulRateLimiterEureka9000Application.java
@@ -0,0 +1,24 @@
+package com.jade.serviceeureka;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy // start zuul proxy pattern
+@SpringCloudApplication
+public class ServiceZuulRateLimiterEureka9000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulRateLimiterEureka9000Application.class, args);
+    }
+
+    // 设置zuul 对consumer 的 负载均衡策略 为 “随机策略”
+    @Bean
+    public IRule loadBalanceRule(){
+        return new RandomRule();
+    }
+
+}
