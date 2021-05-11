2ac07aca2e49d7ffdd36b86181700becbe6ede46
Asurada520 <602423940@qq.com>
2021-05-11 13:39:49 +0800

    spring cloud zuul gray two methods test

--- service-zuul-gray-eureka-9000/src/main/java/com/jade/serviceeureka/filter/Gray2Filter.java
+++ service-zuul-gray-eureka-9000/src/main/java/com/jade/serviceeureka/filter/Gray2Filter.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-//@Component
-public class GrayFilter extends ZuulFilter {
+@Component
+public class Gray2Filter extends ZuulFilter {
 
+    private AtomicBoolean flag = new AtomicBoolean(true);
 
     @Override
     public String filterType() {
@@ -35,13 +37,23 @@ public class GrayFilter extends ZuulFilter {
         RequestContext context = RequestContext.getCurrentContext();
         HttpServletRequest request = context.getRequest();
         // 获取指定的请求头信息，该头信息在浏览器提交请求时携带，用于区分该请求要被路由到哪个主机处理
-        String mark = request.getHeader("gray-mark");
-        // 默认将请求路由到running-host上
-        RibbonFilterContextHolder.getCurrentContext().add("host-mark", "running-host");
-        // 若mark的值不为空且值为enable，则将请求路由到gray-host，其它请求会路由到默认的running-host
-        if (!StringUtils.isEmpty(mark) && "enable".equals(mark)) {
+//        String mark = request.getHeader("gray-mark");
+//        // 默认将请求路由到running-host上
+//        RibbonFilterContextHolder.getCurrentContext().add("host-mark", "running-host");
+//        // 若mark的值不为空且值为enable，则将请求路由到gray-host，其它请求会路由到默认的running-host
+//        if (!StringUtils.isEmpty(mark) && "enable".equals(mark)) {
+//            RibbonFilterContextHolder.getCurrentContext().add("host-mark", "gray-host");
+//        }
+
+        if(flag.get()){
+            // 默认将请求路由到running-host上
+            RibbonFilterContextHolder.getCurrentContext().add("host-mark", "running-host");
+            flag.set(false);
+        }else {
             RibbonFilterContextHolder.getCurrentContext().add("host-mark", "gray-host");
+            flag.set(true);
         }
+
 
         return null;
     }
